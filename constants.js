0847f56058eeef61c9dd8afdc4699da2fe15780b
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-10-06 01:04:15 +0530

    Update Version

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ const env = {
     UPSTREAM_REFRESH_INTERVAL: 300,
     UPSTREAM_URL: "https://konkanrailway.com/VisualTrain/",
     REPO_URL: "https://github.com/sibi361/konkan-railway_api",
-    API_VERSION: 1,
+    API_VERSION: 2,
     PUPPETEER_OPTS: { headless: "false" },
     SERVER_ERROR_MESSAGE: "Server overloaded. Please wait.",
 };
